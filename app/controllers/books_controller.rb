5e8787140681fb3810a73294d9a1c58076de9d13
rainpalace2 <110389642+rainpalace2@users.noreply.github.com>
2022-08-11 13:11:15 +0000

    [Update]RSpecのテスト

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -7,7 +7,7 @@ class BooksController < ApplicationController
     @book = Book.new(book_params)
     if @book.save
       flash[:notice] = "Book was successfully created."
-      redirect_to book_path(@book.id)
+      redirect_to book_path(@book)
     else
        @books = Book.all
        render :index
@@ -30,7 +30,7 @@ class BooksController < ApplicationController
   def update
     @book = Book.find(params[:id])
     if @book.update(book_params)
-    redirect_to book_path(@book.id)
+    redirect_to book_path(@book)
     flash[:notice] = "Book was successfully updated."
     else
       render :edit
@@ -40,7 +40,7 @@ class BooksController < ApplicationController
   def destroy
     book = Book.find(params[:id])
     book.destroy
-    redirect_to'/books'
+    redirect_to books_path
     flash[:notice] = "Book was successfully destroyed."
   end
 
