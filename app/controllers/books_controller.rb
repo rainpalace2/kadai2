db6c383398a1738cb05e3ad1c1dc41d978a95475
rainpalace2 <110389642+rainpalace2@users.noreply.github.com>
2022-08-11 07:22:35 +0000

    [Add]RSpecのテストファイル

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -29,8 +29,7 @@ class BooksController < ApplicationController
 
   def update
     @book = Book.find(params[:id])
-    if @book.save
-
+    if @book.update(book_params)
     redirect_to book_path(@book.id)
     flash[:notice] = "Book was successfully updated."
     else
